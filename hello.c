85b109f11735b6aa61c02420accf2e0bd3437b23
Abdullahi <ayobami.oladosu@gmail.com>
2016-04-01 23:19:08 -0400

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,9 +1,13 @@
+#include <stdlib.h>
 #include <stdio.h>
 
-void sayHello(char* name){
+void sayHello(char *name){
 	printf("Hello, %s\n", name);
 }
 
 int main(){
-	sayHello(scanf("%s"));
+	char* input = malloc(sizeof(char*));
+	scanf("%s", input);
+	sayHello(input);
+	free(input);
 }
